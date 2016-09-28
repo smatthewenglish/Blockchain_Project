005df177001bd480bb62af04f6cc685028f32e39
Ernane Luis <ernane.luis@gmail.com>
2016-09-27 23:59:59 -0300

    transaction graph page taking transaction hash from get url and showing the transaction graph inside of django app

--- SemanticBlockchain/transaction/views.py
+++ SemanticBlockchain/transaction/views.py
@@ -7,4 +7,7 @@ from django.http.response import HttpResponse
 def index(request):
     return HttpResponse('<h1>Graph for Transactions</h1>')
 def transactiongraoh(request,transaction_hsh):
-    return HttpResponse("<h2>Graph for transaction: "+transaction_hsh+"</h2>")
+    # return HttpResponse("<h2>Graph for 1 transaction: "+transaction_hsh+"</h2>")
+    return render(request, 'transaction.html', {
+        'transaction_hash' : transaction_hsh
+    })
