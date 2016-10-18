7d8058f906162fe51c3cefb1cbc7a85ca82a37d9
Ernane Luis <ernane.luis@gmail.com>
2016-10-18 13:25:50 +0200

    Transaction done with rdfa and query from database

--- SemanticBlockchain/transaction/views.py
+++ SemanticBlockchain/transaction/views.py
@@ -15,36 +15,48 @@ def transactiongraoh(request,transaction_hsh):
     graph = py2neo.Graph(neo4jUrl)
 
     # get transaction node
-    query = """
-               MATCH (p:Transaction {hsh:{y}})
-               return p
-               """
+    # query = """
+    #            MATCH (p:Transaction {hsh:{y}})
+    #            return p
+    #            """
 
     # get sender and reciver
-    query2="""
-            MAtch (p:Transaction {hsh:{y}}),(sender:BTC_Address), (receiver:BTC_Address)
-            MATCH (sender)-[r:send]->(p)
-            MATCH (p)-[rr:send]->(receiver)
-            return p,sender,receiver, r,rr
+    # query2="""
+    #         MAtch (p:Transaction {hsh:{y}}),(sender:BTC_Address), (receiver:BTC_Address)
+    #         MATCH (sender)-[r:outputs_to]->(p)
+    #         MATCH (p)-[rr:outputs_to]->(receiver)
+    #         return p,sender,receiver, r,rr
+    # """
+
+    query="""
+        MATCH (senders:BTC_Address)-[inp:Inputs_to]->(tran:Transaction)
+        MATCH (tran:Transaction)-[out:Outputs_to]->(receivers:BTC_Address)
+        where receivers<>senders AND tran.hsh={y}
+        RETURN senders,inp,tran,out,receivers
     """
+
     # Send Cypher query.
 
     q1 = graph.run(query,y="2e9f40d4af66964a526d51233f87c401eaf66e05afd1e65928d2f2a51e636232")
-    transaction_obj = None;
+    transaction_obj = None
+    senders_obj = None
+    receivers_obj = None
+    inputs_obj = None
+    outputs_obj = None
     # print(x)
     for rec in q1:
-        transaction_obj = rec[0]
-
-    q2 = graph.run(query2,y="2e9f40d4af66964a526d51233f87c401eaf66e05afd1e65928d2f2a51e636232")
-    transaction_s = None;
-
-    for rec2 in q2:
-        print(rec2)
-        transaction_s = rec2[0]
-
+        transaction_obj = rec["tran"]
+        senders_obj     =  rec["senders"]
+        receivers_obj   = rec["receivers"]
+        inputs_obj     =  rec["inp"]
+        outputs_obj   = rec["out"]
 
 
     return render(request, 'transaction.html', {
         'transaction_hash' : transaction_hsh,
-        'transaction' : transaction_obj
+        'transaction' : transaction_obj,
+        'senders' : senders_obj,
+        'receivers' : receivers_obj,
+        'inputs' : inputs_obj,
+        'outputs' : outputs_obj,
     })
