bca6222657caac55aa97ac2a7897b0c6d0fdb29c
Ernane Luis <ernane.luis@gmail.com>
2016-10-19 03:46:47 +0200

    bugfix

--- SemanticBlockchain/ooverview/api.py
+++ SemanticBlockchain/ooverview/api.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
-from wrappers.V2CypherWrapper import get_Transaction
+# from wrappers.V2CypherWrapper import get_Transaction
 
 def addTx2graph(request):
-	transactionInfo="Unknown transaction" # in case the Blockchain api fails to retreive the tx
-	if request.method=='POST':
-		tx=request.POST['tx']
-		transactionInfo=get_Transaction(tx)
+	# transactionInfo="Unknown transaction" # in case the Blockchain api fails to retreive the tx
+	# if request.method=='POST':
+	# 	tx=request.POST['tx']
+	# 	transactionInfo=get_Transaction(tx)
 		#graph=graphwrapper()
 		#addInputAddresses(transactionInfo,graph)
 		#addOutputAddresses(transactionInfo,graph)
